db2a7670a76f09b227e51bbfd5b9a2e8798dcafd
Tariho617 <20227022-horitayuta@hcs.ac.jp>
2024-05-13 12:07:19 +0900

    constants.php更新

--- SampleProject/config/constants.php
+++ SampleProject/config/constants.php
@@ -5,5 +5,8 @@ return array(
     'JEWEL_FREE_DEFAULT' => 10,
     'FRIEND_COIN_DEFAULT' => 0,
     'TUTORIAL_START' => 0,
+    'ITEM_TYPE_JEWEL' => 1,
+    'ITEM_TYPE_JEWEL_FREE' => 2,
+    'ITEM_TYPE_FRIEND_COIN' => 3,
 );
 
