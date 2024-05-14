a00481c90bb24274c2fbf174854eb833039f88e0
Tariho617 <20227022-horitayuta@hcs.ac.jp>
2024-05-14 14:45:27 +0900

    MasterLoginItem修正

--- SampleProject/app/Models/MasterLoginItem.php
+++ SampleProject/app/Models/MasterLoginItem.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Libs\MasterDataService;
 
 class MasterLoginItem extends Model
 {
